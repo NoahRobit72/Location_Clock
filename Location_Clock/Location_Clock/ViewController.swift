17bf468423c70d0bd9aab3a058f094dc5c9b5782
Noah Robitshek <64294283+NoahRobit72@users.noreply.github.com>
2022-12-23 21:38:01 -0500

    Initial Commit

---
+++ Location_Clock/Location_Clock/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Location_Clock
+//
+//  Created by Noah Robitshek on 12/23/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
