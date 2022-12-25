143464628fcf1daeb37c589efaa536c5f846f2a2
Noah Robitshek <64294283+NoahRobit72@users.noreply.github.com>
2022-12-24 22:47:58 -0500

    added timer test

---
+++ Timer_Test/Timer_test/Timer_test/Timer_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  Timer_testApp.swift
+//  Timer_test
+//
+//  Created by Noah Robitshek on 12/24/22.
+//
+
+import SwiftUI
+
+@main
+struct Timer_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
